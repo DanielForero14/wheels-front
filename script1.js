3dd9c7ef765110c5ca8a700e024a8bbc41818ac2
DanielForero14 <danielforsa@unisabana.edu.co>
2024-10-25 07:15:18 -0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        window.location.href = "/Login-nosql-app/frontend/html/index3.html";  // O la ruta que necesites
+    }, 3000);
+});
