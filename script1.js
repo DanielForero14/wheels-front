0eab1fb8d342905ce62321b4b394aeae0cfd542b
DanielForero14 <danielforsa@unisabana.edu.co>
2024-10-25 07:47:58 -0500

    123

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        window.location.href = "/Login-nosql-app/frontend/html/index3.html";  // O la ruta que necesites
+        window.location.href = "index3.html";  // O la ruta que necesites
     }, 3000);
 });
