648c8f4e41dcbc60d65f54986c70a34ae5c13aa2
DanielForero14 <danielforsa@unisabana.edu.co>
2024-11-20 07:03:20 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        window.location.href = "./Login-app/frontend/html/Index3.html";  // O la ruta que necesites
+        window.location.href = "/Login-app/frontend/html/Index3.html";  // O la ruta que necesites
     }, 3000);
 });
